0b67c493a7262a7a35edbec46604ab08df393ab7
Finnbur <burghaus.finn@gmail.com>
2026-03-09 21:37:40 +0100

    added a picture box and form1 load

--- APPR_TickTackChess_24SD_Finn/Form1.cs
+++ APPR_TickTackChess_24SD_Finn/Form1.cs
@@ -16,5 +16,10 @@ namespace APPR_TickTackChess_24SD_Finn
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
